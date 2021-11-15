8a9dab2a328c26c41c84813bf342d9d31240d527
Luke Lancaster <luketlancaster@gmail.com>
2021-11-15 16:54:50 -0600

    Icon Input thingie closeish
    Still needs:
    - [ ] Click the icon to focus the input
    - [ ] Size support probs?
    - [ ] Other things?

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,58 @@
-import React from 'react';
-import styled from 'styled-components';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import React from "react";
+import VisuallyHidden from "../VisuallyHidden";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const style = {
+    "--width": width + "px",
+    "--borderPx": size === "large" ? "2px" : "1px",
+  };
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+  return (
+    <WrapperUI>
+      <IconWrapperUI>
+        <Icon id={icon} size={16} />
+      </IconWrapperUI>
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+      <InputUI
+        placeholder={placeholder}
+        width={width}
+        style={style}
+        type="text"
+      />
+
+      <VisuallyHidden>
+        <label>{label}</label>
+      </VisuallyHidden>
+    </WrapperUI>
+  );
 };
+
+const WrapperUI = styled.div`
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const InputUI = styled.input`
+  font-family: "Roboto", sans-serif;
+  width: var(--width);
+  border: none;
+  border-bottom: var(--borderPx) solid ${COLORS.black};
+  padding-left: 20px;
+  color: ${COLORS.gray700};
+  font-weight: bold;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: normal;
+  }
+`;
+
+const IconWrapperUI = styled.div`
+  position: absolute;
+`;
 
 export default IconInput;
